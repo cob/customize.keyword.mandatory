5484d3e76973472f4fb64843980df22d4c6b136f
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2023-07-05 09:31:45 +0100

    Initial version of mandatoryIf custom validator

---
+++ others/recordm-validators/src/main/java/com/cultofbits/customize/mandatoryif/validators/MandatoryIfValidator.java
@@ -0,0 +1,162 @@
+package com.cultofbits.customize.mandatoryif.validators;
+
+import com.cultofbits.recordm.core.model.FieldKeyArguments;
+import com.cultofbits.recordm.core.model.Instance;
+import com.cultofbits.recordm.core.model.InstanceField;
+import com.cultofbits.recordm.customvalidators.api.AbstractOnCreateValidator;
+import com.cultofbits.recordm.customvalidators.api.ErrorType;
+import com.cultofbits.recordm.customvalidators.api.OnUpdateValidator;
+import com.cultofbits.recordm.customvalidators.api.ValidationError;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.cultofbits.recordm.customvalidators.api.ValidationError.custom;
+import static com.cultofbits.recordm.customvalidators.api.ValidationError.standard;
+
+public class MandatoryIfValidator extends AbstractOnCreateValidator implements OnUpdateValidator {
+
+    public static final String KEYWORD = "$mandatoryIf";
+
+    protected static final Pattern EXPRESSION_PATTERN = Pattern.compile("(.*?)(=|!|>|<|$)(.*)");
+
+    @SuppressWarnings("UnstableApiUsage")
+    private static final LoadingCache<String, Expr> EXPRESSION_CACHE_BUILDER = CacheBuilder.newBuilder()
+            .maximumSize(100)
+            .expireAfterWrite(24, TimeUnit.HOURS)
+            .build(CacheLoader.from(MandatoryIfValidator::buildExpression));
+
+    @Override
+    public Collection<ValidationError> onCreate(Instance instance) {
+        return validateInstanceFields(instance.getRootFields());
+    }
+
+    @Override
+    public Collection<ValidationError> onUpdate(Instance persistedInstance, Instance updatedInstance) {
+        return validateInstanceFields(updatedInstance.getRootFields());
+    }
+
+    public Collection<ValidationError> validateInstanceFields(List<InstanceField> instanceFields) {
+        List<ValidationError> errors = new ArrayList<>();
+
+        for (InstanceField instanceField : instanceFields) {
+            if ((!instanceField.isVisible() || instanceField.getValue() != null)
+                && instanceField.children.isEmpty()) {
+                continue;
+            }
+
+            if (!instanceField.fieldDefinition.containsExtension(KEYWORD)) {
+                errors.addAll(validateInstanceFields(instanceField.children));
+                continue;
+            }
+
+            if (instanceField.children.isEmpty()
+                    && (instanceField.getValue() != null || !instanceField.fieldDefinition.containsExtension(KEYWORD)))
+                continue;
+
+            FieldKeyArguments keywordArgs = instanceField.fieldDefinition.argsFor(KEYWORD);
+            String mandatoryArg = !(keywordArgs instanceof FieldKeyArguments.None) ? keywordArgs.get().get(0) : "";
+
+            //noinspection UnstableApiUsage
+            Expr expr = EXPRESSION_CACHE_BUILDER.getUnchecked(mandatoryArg);
+
+            try {
+                if (expr.fieldName == null || expr.isTrue(instanceField.getClosest(expr.fieldName))) {
+                    errors.add(standard(instanceField, ErrorType.MANDATORY));
+                }
+            } catch (Exception e) {
+                errors.add(custom(instanceField, "Error validating mandatory expression: " + mandatoryArg));
+            }
+
+            if (instanceField.children.size() > 0) {
+                errors.addAll(validateInstanceFields(instanceField.children));
+            }
+        }
+
+        return errors;
+    }
+
+    protected static Expr buildExpression(String arg) {
+        if (arg == null || arg.trim().isEmpty()) {
+            return new Expr();
+        }
+
+        Matcher expMatcher = EXPRESSION_PATTERN.matcher(arg);
+        if (!expMatcher.matches()) {
+            throw new IllegalStateException("The expression pattern should have matched. {{"
+                    + "expression:" + arg + "}}");
+        }
+
+        return new Expr(expMatcher.group(1), expMatcher.group(2), expMatcher.group(3));
+    }
+
+    protected static class Expr {
+        private String fieldName;
+        private String operation;
+        private String value;
+
+        public Expr() {
+        }
+
+        public Expr(String fieldName, String operation, String value) {
+            this.fieldName = fieldName.trim();
+            this.operation = operation != null && !operation.isEmpty() ? operation.trim() : null;
+            this.value = value != null && !value.isEmpty() ? value.trim() : null;
+        }
+
+        public boolean isTrue(InstanceField sourceField) {
+            String fieldValue = sourceField.getValue();
+
+            if ("=".equals(operation)) {
+                return (value == null && fieldValue == null) // both are null
+                        || (value != null && value.equals(fieldValue));
+
+            } else if ("!".equals(operation)) {
+                return (value == null && fieldValue != null) // both are null
+                        || (value != null && !value.equals(fieldValue));
+
+            } else if (">".equals(operation)) {
+                return value != null && fieldValue != null
+                        && Float.parseFloat(fieldValue) > Float.parseFloat(value);
+
+            } else if ("<".equals(operation)) {
+                return value != null && fieldValue != null
+                        && Float.parseFloat(fieldValue) < Float.parseFloat(value);
+            }
+
+            return false;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Expr expr = (Expr) o;
+            return Objects.equals(fieldName, expr.fieldName)
+                    && Objects.equals(operation, expr.operation)
+                    && Objects.equals(value, expr.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(fieldName, operation, value);
+        }
+
+        @Override
+        public String toString() {
+            return "Expr{" +
+                    "fieldName='" + fieldName + '\'' +
+                    ", operation='" + operation + '\'' +
+                    ", value='" + value + '\'' +
+                    '}';
+        }
+    }
+}
---
+++ others/recordm-validators/src/test/java/com/cultofbits/customize/mandatoryif/validators/MandatoryIfValidatorTest.java
@@ -0,0 +1,204 @@
+package com.cultofbits.customize.mandatoryif.validators;
+
+import com.cultofbits.recordm.core.model.Definition;
+import com.cultofbits.recordm.core.model.FieldDefinition;
+import com.cultofbits.recordm.core.model.Instance;
+import com.cultofbits.recordm.core.model.InstanceField;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class MandatoryIfValidatorTest {
+
+    private final MandatoryIfValidator validator = new MandatoryIfValidator();
+
+
+    @Test
+    public void can_build_expressions() {
+        assertEquals(MandatoryIfValidator.buildExpression(null),
+                     new MandatoryIfValidator.Expr());
+
+        assertEquals(MandatoryIfValidator.buildExpression(""),
+                     new MandatoryIfValidator.Expr());
+
+        assertEquals(MandatoryIfValidator.buildExpression("     "),
+                     new MandatoryIfValidator.Expr());
+
+        assertEquals(MandatoryIfValidator.buildExpression("akjbdf % akjfcb"),
+                     new MandatoryIfValidator.Expr("akjbdf % akjfcb", null, null));
+
+        assertEquals(MandatoryIfValidator.buildExpression("field name"),
+                     new MandatoryIfValidator.Expr("field name", null, null));
+
+        assertEquals(MandatoryIfValidator.buildExpression("field name = test"),
+                     new MandatoryIfValidator.Expr("field name", "=", "test"));
+
+        assertEquals(MandatoryIfValidator.buildExpression("field!"),
+                     new MandatoryIfValidator.Expr("field", "!", null));
+
+        assertEquals(MandatoryIfValidator.buildExpression("field=1"),
+                     new MandatoryIfValidator.Expr("field", "=", "1"));
+    }
+
+    @Test
+    public void pass_validation_if_condition_fails() {
+        Instance instance = anInstance(
+            aField("User Type", "$[Robot,User]", "Robot"),
+            aField("Address", "$mandatoryIf(User Type=User)", null));
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).isEmpty());
+    }
+
+    @Test
+    public void pass_validation_if_field_is_not_empty() {
+        Instance instance = anInstance(
+            aField("User Type", "$[Robot,User]", "Robot"),
+            aField("Address", "$mandatoryIf(User Type=)", null));
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).isEmpty());
+    }
+
+    @Test
+    public void pass_validation_if_condition_true_and_field_has_value() {
+        Instance instance = anInstance(
+            aField("User Type", "$[Robot,User]", "User"),
+            aField("Address", "$mandatoryIf(User Type=User)", "an address"));
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).isEmpty());
+    }
+
+    @Test
+    public void fail_validation_when_condition_is_true() {
+        Instance instance = anInstance(
+            aField("User Type", "$[Robot,User]", "User"),
+            aField("Address", "$mandatoryIf(User Type=User)", null));
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).size() > 0);
+    }
+
+    @Test
+    public void fail_validation_when_target_field_is_not_empty() {
+        Instance instance = anInstance(
+            aField("User Type", "$[Robot,User]", "User"),
+            aField("Address", "$mandatoryIf(User Type!)", null));
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).size() > 0);
+    }
+
+    @Test
+    public void fail_validation_if_no_condition_and_value_is_null() {
+        Instance instance = anInstance(
+            aField("Address", "$mandatoryIf", null));
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).size() > 0);
+    }
+
+    @Test
+    public void fail_validation_if_range_condition_is_false() {
+        Instance instance = anInstance(
+            aField("Distance", "$number", "0"),
+            aField("Message", "$mandatoryIf(Distance > 10)", null));
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).isEmpty());
+    }
+
+    @Test
+    public void fail_validation_if_range_condition_is_true() {
+        Instance instance = anInstance(
+            aField("Distance", "$number", "0"),
+            aField("Message", "$mandatoryIf(Distance < 10)", null));
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).size() > 0);
+    }
+
+    @Test
+    public void pass_validation_if_not_greater_than() {
+        Instance instance = anInstance(
+            aField("Number", "$[1,2,3,4,5]", null),
+            aField("Message", "$mandatoryIf(Number < 2)", null));
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).isEmpty());
+    }
+
+    @Test
+    public void can_validate_instances_with_groups() {
+        InstanceField field1 = aField("User Type", "$[Robot,User]", "User");
+        InstanceField field2 = aField("Address", "$mandatoryIf(User Type!)", null);
+
+        InstanceField groupField = aField("Group", "$group", "3");
+        groupField.children = Arrays.asList(field1, field2);
+
+        Instance instance = anInstance(groupField, field1, field2);
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).size() > 0);
+    }
+
+    @Test
+    public void can_validate_instances_with_children() {
+        InstanceField usernameField = aField("username", "$mandatoryIf(User Type=User)", null);
+        InstanceField addressField = aField("Address", "$mandatoryIf(User Type!)", null);
+
+        InstanceField parentField = aField("User Type", "$[Robot,User]", "User");
+        parentField.children = Arrays.asList(usernameField, addressField);
+
+        Instance instance = anInstance(parentField, usernameField, addressField);
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).size() > 0);
+    }
+
+    @Test
+    public void can_validate_instances_with_children_but_fail_condition() {
+        InstanceField usernameField = aField("username", "$mandatoryIf(User Type=User)", null);
+
+        InstanceField parentField = aField("User Type", "$[Robot,User]", "Robot");
+        parentField.children = Collections.singletonList(usernameField);
+
+        Instance instance = anInstance(parentField, usernameField);
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).isEmpty());
+    }
+
+    @Test
+    public void can_validate_instances_with_parent_with_null_value_and_fail_condition() {
+        InstanceField usernameField = aField("username", "$mandatoryIf(User Type=User)", null);
+
+        InstanceField parentField = aField("User Type", "$[Robot,User]", null);
+        parentField.children = Collections.singletonList(usernameField);
+
+        Instance instance = anInstance(parentField, usernameField);
+
+        assertTrue(validator.validateInstanceFields(instance.getFields()).isEmpty());
+    }
+
+    public static Instance anInstance(InstanceField... fields) {
+        Instance instance = new Instance();
+        instance.definition = new Definition(1, "A Definition");
+
+        instance.fields = Arrays.asList(fields);
+        instance.definition.setFieldDefinitions(new ArrayList<>(
+            instance.fields.stream()
+                .peek(f -> f.instance = instance)
+                .map(f -> f.fieldDefinition)
+                .peek(fd -> fd.definition = instance.definition)
+                .collect(Collectors.toSet())
+        ));
+
+        return instance;
+    }
+
+    public static InstanceField aField(String name, String description, String value) {
+        FieldDefinition fieldDefinition = new FieldDefinition(null, name, null, description, null);
+        InstanceField instanceField = new InstanceField();
+        instanceField.fieldDefinition = fieldDefinition;
+        instanceField.setValue(value);
+
+        return instanceField;
+    }
+
+}
